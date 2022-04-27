aeaf666c489b1b052509a1524ae7cef36f3edd0f
Robel Kassa <rkassa@rockfound.org>
2022-04-27 18:24:52 -0400

    color palette and legend added

--- src/components/map.js
+++ src/components/map.js
@@ -1,21 +1,15 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import Filters from './filters.js';
-// import WorldData from 'https://ppi-estimator.s3.amazonaws.com/data.fc.fgb';
-import geobuf from 'geobuf';
-import { PublicOff } from '@mui/icons-material';
   
 mapboxgl.accessToken = 'pk.eyJ1IjoidG9vdGhwaWNrIiwiYSI6ImNpczVlajdoODBhcXkyc28wdWJvOWhpcTUifQ.AU8-RSZrTkWidj7yDLPtMg';
-// var geojson = geobuf.decode(new PublicOff(WorldData));
  
 export default function Map() {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(12);
-    const [lat, setLat] = useState(30);
+    const [lng, setLng] = useState(0);
+    const [lat, setLat] = useState(45);
     const [zoom, setZoom] = useState(1.5);
-    // let hoveredStateId = null;
-    // console.log("Hello big guy: ", geojson);
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
@@ -42,39 +36,44 @@ export default function Map() {
                 'paint': {
                     'fill-color': {
                         'property': 'risk',
-                        'stops': [[0, 'YELLOW'], [1, 'pink'], [5, 'red']]
+                        'stops': [[0, '#efefef'], [1, '#d9ed92'], [25, '#99d98c'], [50, '#52b69a'], [75, '#168aad'], [99, '#1e6091']]
                       },
                     'fill-opacity': [
                         'case',
                         ['boolean', ['feature-state', 'hover'], false],
                         1,
-                        0.5
+                        1
                     ],
                     'fill-antialias': true,
-                    'fill-outline-color': '#000000',
+                    'fill-outline-color': {
+                        'property': 'risk',
+                        'stops':  [[0, '#cccccc'], [1, '#b5e48c'], [25, '#76c893'], [50, '#34a0a4'], [75, '#1a759f'], [99, '#1e6091']]
+                      },
                 },
                 'filter': ['==', '$type', 'Polygon']
             }, 'road-simple');  
+
+            var popup = new mapboxgl.Popup({ offset: [0, -7] });
+                    
             
-            map.current.on("mousemove", function (e) {
+            map.current.on('click', 'world', function(e) {
+                map.current.getCanvas().style.cursor = 'pointer';
                 var features = map.current.queryRenderedFeatures(e.point, {
-                    layers: ["world"]
+                    layers: ['world'] 
                 });
-    
-                if (features.length) {
-                    //show name and value in sidebar
-                    document.getElementById('tooltip-name').innerHTML = features[0].properties.RegionName;
-                    document.getElementById('tooltip').innerHTML = features[0].properties.DateReport;
-                    //for troubleshooting - show complete features info
-                    //document.getElementById('tooltip').innerHTML = JSON.stringify(features, null, 2);
-                } else {
-                    //if not hovering over a feature set tooltip to empty
-                    document.getElementById('tooltip-name').innerHTML = "";
-                    document.getElementById('tooltip').innerHTML = "";
+        
+                if (!features.length) {
+                    return;
                 }
+        
+                var feature = features[0];
+
+                popup
+                .setLngLat(e.lngLat)
+                .setHTML('<h3>' + feature.properties.RegionName + '</h3><p>' + feature.properties.risk + '</p>' + '<h3>' + feature.properties.DateReport + '</h3><p>' + feature.properties.country + '</p>' )
+                .addTo(map.current);
             });
     
-
         });
             
     });
@@ -97,8 +96,16 @@ export default function Map() {
                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
             </div>
             <div ref={mapContainer} className="map-container" />
-            <div id="tooltip-name"></div>
-            <div id='tooltip'></div>
+            <div id="mapLegend">
+                <h5>Probability Estimate for Exposure Risk (%)</h5>
+                <span class="range1">Less than 1% </span>
+                <span class="range2">1 - 25 </span>
+                <span class="range3">25 - 50 </span>
+                <span class="range4">50 - 75 </span>
+                <span class="range5">75 - 99 </span>
+                <span class="range6">More than 99% </span>
+                <span class="nodata">No Data</span>
+            </div>
         </div>
     );
 }
