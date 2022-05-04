59f2db0ee414c491da9bfbd277e7971a07fca347
Mina Parastaran <mmparastaran@email.wm.edu>
2022-05-04 12:25:40 -0400

    Moved styling to css file and fixed button squashing

--- src/components/filters.js
+++ src/components/filters.js
@@ -97,7 +97,7 @@ export default function Filters() {
             <h3 className="serif">Select your event location and size</h3>
             <p>Where will the event or activity take place and how many people will be attending?</p>
 
-            <h4><RoomOutlined fontSize="small" style={{ color: '#318AD0', marginBottom: -3 }} /> LOCATION</h4>
+            <h4><RoomOutlined className={styles.roomOutlined}/> LOCATION</h4>
             <TextField 
                 fullWidth
                 id="outlined-basic" 
@@ -105,7 +105,7 @@ export default function Filters() {
                 variant="outlined" 
             />
 
-            <h4 style={{marginBottom: 40}}><PeopleAltOutlined fontSize="small" style={{ color: '#318AD0', marginBottom: -3 }} /> CROWD SIZE</h4>
+            <h4 className={styles.crowdSize}><PeopleAltOutlined className={styles.peopleAltOutlined}/> CROWD SIZE</h4>
             <Slider
                 aria-label="Restricted values"
                 defaultValue={10}
@@ -119,45 +119,45 @@ export default function Filters() {
         </div>
         
         <div id='Precautions'>
-          <h4 style={{marginBottom: 20}}>
-            <ShieldOutlined fontSize="small" style={{ color: '#318AD0', marginBottom: -3, marginRight: 5 }} /> TAKE THE RIGHT PRECAUTIONS
+          <h4 className={styles.precautionBlock}>
+            <ShieldOutlined className={styles.shieldOutlined}/> TAKE THE RIGHT PRECAUTIONS
             <IconButton aria-label="open precautions" className={styles.expandButton} onClick={open ? handlePrecautionsClose : handlePrecautionsOpen}>
-              <ExpandMoreOutlined style={{ color: '#6C787F', marginBottom: -5, display: !open ? 'block' : 'none' }} />
-              <ExpandLessOutlined style={{ color: '#6C787F', marginBottom: -5, display: open ? 'block' : 'none' }} />
+              <ExpandMoreOutlined className={styles.expandOutlined} style={{ display: !open ? 'block' : 'none' }} />
+              <ExpandLessOutlined className={styles.expandOutlined} style={{ display: open ? 'block' : 'none' }} />
             </IconButton>
           </h4>
           <div className={styles.iconGrid} style={{display: open ? 'grid' : 'none'}}>
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={mask} alt="Use a mask" />
               </div>
               <div className={styles.precautionText}>Use a mask</div>
             </div>  
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={socialDistance} alt="Social distance" />
               </div>
               <div className={styles.precautionText}>Social distance</div>
             </div> 
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={handwashing} alt="Wash your hands" />
               </div>
               <div className={styles.precautionText}>Wash your hands</div>
             </div>  
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={outdoors} alt="Choose the outdoors" />
               </div>
               <div className={styles.precautionText}>Choose the outdoors</div>
             </div> 
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={sanitizer} alt="Use hand sanitizer" />
               </div>
               <div className={styles.precautionText}>Use hand sanitizer</div>
             </div>  
-            <div style={{display: 'block'}}>
+            <div className={styles.iconBlock}>
               <div className={styles.circle}>
                 <img src={vaccines} alt="Get vaccinated" />
               </div>
