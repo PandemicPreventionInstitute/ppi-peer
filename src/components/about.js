ec984c6a8648bab9c7a06957cbd9d916c1e7194c
Mina Parastaran <mmparastaran@email.wm.edu>
2022-05-12 19:30:05 -0400

    Adding back in large masked elbow image, changing 3-card view on xl screens, adjusting misc styling

--- src/components/about.js
+++ src/components/about.js
@@ -12,6 +12,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import creditCard from '../assets/credit_card.png';
 import calculator from '../assets/calculator.png';
+import camera from '../assets/camera.png';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -53,7 +54,7 @@ export default function About() {
     
     return (
         <Grid sx={{ flexGrow: 1 }} container>
-            <Grid item xs={12}>
+            <Grid item xs={12} xl={6}>
                 <div>
                     <h1 className={styles.mainText}>PEER - Probability Estimator for Exposure Risk:</h1>
                     <h3 className={styles.subText}>A COVID-19 calculator powered by the PPI and Georgia Tech</h3>
@@ -67,16 +68,19 @@ export default function About() {
                     </p>
                 </div>              
             </Grid>
+            <Grid item xs={0} sm={0} md={0} lg={12} xl={6} sx={{textAlign: 'center'}}>
+                <img className={styles.maskedElbowBumpLarge} src={maskedElbowBump} alt='Masked elbow bump' />
+            </Grid>
             <Grid item xs={12} sm={12} sx={{height: 'auto', marginBottom: '5px'}} >
                 <div>
                     <h3 className={styles.transitionText}>We envision a world with...</h3>
                 </div>
             </Grid>
-            <Grid item xs={12} sm={12} className={styles.bottomGridBox}>
+            <Grid item xs={12} sm={12} xl={4} className={styles.bottomGridBox}>
                 <div>
                     <VisionCard>    
-                        <CardContent >
-                            <img className={styles.maskedElbowBumpSmall} src={maskedElbowBump} alt='Masked elbow bump' />
+                        <CardContent sx={{height: '230px'}}>
+                            <img className={styles.camera} src={camera} alt='Camera' />
                             <h3 className={styles.visionsText}>
                               A connected, sustainable pandemic data ecosystem
                                 <ExpandMore
@@ -92,36 +96,32 @@ export default function About() {
                         <Collapse in={expandedConnect} timeout="auto" unmountOnExit>
                             <CardContent>
                                 <Typography paragraph>
-                                    <div>
-                                        <p>Countless impactful innovations developed by academic researchers struggle to reach sustainable 
-                                            application at-scale. In the fields of pandemic preparedness and communicable disease, this 
-                                            problem is only more pronounced. Further, these fields need sustainable coordination and innovation 
-                                            but are often fragmented and underfunded, leaving many projects competing with one another 
-                                            and without long-term homes.
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p>Our collaboration with Georgia Tech seeks to take on this challenge. 
-                                            Precisely because of its public-facing impact ({'>'}16M visitors and {'>'}60M risk estimates thus far), 
-                                            the Event Risk Assessment Planning Tool by Georgia Tech has highlighted how investments in pandemic 
-                                            data collection and integration can be communicated to the public with interpretable and actionable 
-                                            real-time metrics. We also want to spotlight shortcomings in similar global tools (such as, data 
-                                            dashboards) to convey the time-sensitive opportunity to expand such approaches globally. 
-                                            Rather than introduce another tool, our hope is that the PPI can build on what already exists in the 
-                                            Event Risk Tool by focusing on user-centered product design while enabling Georgia Tech to focus 
-                                            on advancing the science of COVID-19 prevalence estimation and risk assessment.
-                                        </p>
-                                    </div>  
+                                    Countless impactful innovations developed by academic researchers struggle to reach sustainable 
+                                    application at-scale. In the fields of pandemic preparedness and communicable disease, this 
+                                    problem is only more pronounced. Further, these fields need sustainable coordination and innovation 
+                                    but are often fragmented and underfunded, leaving many projects competing with one another 
+                                    and without long-term homes.
+                                </Typography>
+                                <Typography paragraph>
+                                    Our collaboration with Georgia Tech seeks to take on this challenge. 
+                                    Precisely because of its public-facing impact ({'>'}16M visitors and {'>'}60M risk estimates thus far), 
+                                    the Event Risk Assessment Planning Tool by Georgia Tech has highlighted how investments in pandemic 
+                                    data collection and integration can be communicated to the public with interpretable and actionable 
+                                    real-time metrics. We also want to spotlight shortcomings in similar global tools (such as, data 
+                                    dashboards) to convey the time-sensitive opportunity to expand such approaches globally. 
+                                    Rather than introduce another tool, our hope is that the PPI can build on what already exists in the 
+                                    Event Risk Tool by focusing on user-centered product design while enabling Georgia Tech to focus 
+                                    on advancing the science of COVID-19 prevalence estimation and risk assessment.
                                 </Typography>
                             </CardContent>
                         </Collapse>
                     </VisionCard>
                 </div>                
             </Grid>
-            <Grid item xs={12} sm={12} className={styles.bottomGridBox}>
+            <Grid item xs={12} sm={12} md={12} xl={4} className={styles.bottomGridBox}>
                 <div>
                     <VisionCard>
-                        <CardContent>
+                        <CardContent sx={{height: '230px'}}>
                             <img className={styles.creditCard} src={creditCard} alt="Credit card" ></img>
                             <h3 className={styles.visionsText}>
                             Actionable, interpretable public health tools for all
@@ -138,35 +138,31 @@ export default function About() {
                         <Collapse in={expandedTools} timeout="auto" unmountOnExit>
                             <CardContent>
                                 <Typography paragraph>
-                                    <div>
-                                        <p>The situation with COVID-19 is constantly changing. Restrictions and mask mandates have been rescinded 
-                                        in places, and yet we know novel variants will continue to drive surges of infection. 
-                                        Without clear policy guidelines to inform behavior, the onus is on individuals to determine when 
-                                        COVID-19 poses a threat, and when it doesn’t. To navigate this uncertainty, this data-driven tool – 
-                                        PEER – helps users stay safe while returning a sense of normalcy to their everyday lives.
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p>This is only the beginning. Just as we use probabilistic forecasts of the weather to decide if we need 
-                                        to carry an umbrella or avoid the roads altogether, decision-makers from neighborhoods to nations 
-                                        need tools to make real-time, data-driven, evaluations regarding COVID-19. Envision checking your 
-                                        smartphone, being presented with the risk that someone would arrive infected to an event you are 
-                                        planning, and using this information to decide whether to move the event outdoors, require a 
-                                        pre-event COVID-19 test, or cancel altogether. The PEER COVID-19 Calculator is a way of demonstrating 
-                                        the possibility of a world where decision-support tools around infectious 
-                                        disease risk become as well-understood and accessible as hurricane tracking or traffic alerts.
-                                        </p>
-                                    </div>  
+                                    The situation with COVID-19 is constantly changing. Restrictions and mask mandates have been rescinded 
+                                    in places, and yet we know novel variants will continue to drive surges of infection. 
+                                    Without clear policy guidelines to inform behavior, the onus is on individuals to determine when 
+                                    COVID-19 poses a threat, and when it doesn’t. To navigate this uncertainty, this data-driven tool – 
+                                    PEER – helps users stay safe while returning a sense of normalcy to their everyday lives.
+                                </Typography>
+                                <Typography paragraph>
+                                    This is only the beginning. Just as we use probabilistic forecasts of the weather to decide if we need 
+                                    to carry an umbrella or avoid the roads altogether, decision-makers from neighborhoods to nations 
+                                    need tools to make real-time, data-driven, evaluations regarding COVID-19. Envision checking your 
+                                    smartphone, being presented with the risk that someone would arrive infected to an event you are 
+                                    planning, and using this information to decide whether to move the event outdoors, require a 
+                                    pre-event COVID-19 test, or cancel altogether. The PEER COVID-19 Calculator is a way of demonstrating 
+                                    the possibility of a world where decision-support tools around infectious 
+                                    disease risk become as well-understood and accessible as hurricane tracking or traffic alerts.
                                 </Typography>
                             </CardContent>
                         </Collapse>
                     </VisionCard>
                 </div>                
             </Grid>
-            <Grid item xs={12} sm={12} className={styles.bottomGridBox}>
+            <Grid item xs={12} sm={12} md={12} xl={4} className={styles.bottomGridBox}>
                 <div>
                     <VisionCard>
-                        <CardContent>
+                        <CardContent sx={{height: '230px'}}>
                             <img className={styles.calculator} src={calculator} alt="Calculator" ></img>
                             <h3 className={styles.visionsText}>
                                 Data for the entire globe
@@ -183,15 +179,12 @@ export default function About() {
                         <Collapse in={expandedData} timeout="auto" unmountOnExit>
                             <CardContent>
                                 <Typography paragraph>
-                                    <div>
-                                        <p>Just as clearly as the PEER COVID-19 calculator presents the opportunities available with high quality 
+                                    Just as clearly as the PEER COVID-19 calculator presents the opportunities available with high quality 
                                     and interpretable data, it highlights what stands in our way. We are unable to make risk calculations 
                                     for so many countries in the world because the data that’s needed doesn’t exist! This tool serves 
                                     as a reminder that while many high-income countries in North America and Europe live in relative 
                                     awareness of the trajectory of COVID-19, most of the world does not. We hope PEER will help 
                                     spur increased data sharing to enable more equitable access to tools like these.
-                                        </p>
-                                    </div>
                                 </Typography>
                             </CardContent>
                         </Collapse>
